f375891f281e7671d48eb0c6deeb5c5103ee2a1f
Jessica Perez <1jessicaperez@gmail.com>
2020-06-11 21:30:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist
 }
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.["Slow Dive"]
+  delete playlist["Slow Dive"]
   return playlist
 }
